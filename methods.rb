222c25c774a7838508cb91f66e34b1739775b1a6
Darian Fultz <dfultz826@yahoo.com>
2023-01-31 15:05:07 -0500

    Completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+#2 test
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+#3 tests
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+#4 returns sum of 2 numbers
+def add(num1, num2)
+    return num1 + num2
+end
+
+#5 halve am integer
+def halve(number)
+    if number.class != Integer
+        return nil
+    end
+    return number/2
+end
+
